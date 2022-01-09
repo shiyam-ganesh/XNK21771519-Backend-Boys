4b984f884d487a795b8910b89c037188aa815735
shiyam-ganesh <93067919+shiyam-ganesh@users.noreply.github.com>
2022-01-09 05:54:00 +0530

    Update excel_to_json_converter.py

--- excel_to_json_converter.py
+++ excel_to_json_converter.py
@@ -1,7 +1,7 @@
 import pandas
 import json
 # Read excel document using pandas as a dataframe
-exceldata_df = pandas.read_excel('Book2.xlsx', sheet_name='Sheet1')
+exceldata_df = pandas.read_excel('EXCEL FILE NAME HERE', sheet_name='Sheet1')
 
 # Convert excel to string - orientation is from up to down
 json_string = exceldata_df.to_json(orient='records')
@@ -10,5 +10,5 @@ json_string = exceldata_df.to_json(orient='records')
 json_list = json.loads(json_string)
 
 # Define 
-with open('data_ev.json', 'w') as json_file:
+with open('FILENAME.json', 'w') as json_file:
     json.dump(json_list, json_file)
